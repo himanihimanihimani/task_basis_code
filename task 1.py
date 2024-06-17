58109a47b1e0a8ffe7796d3e44bae837772b0f10
himani <sapkotahimani59@gmail.com>
2024-06-17 07:42:41 +0545

    initial_commit

---
+++ task 1.py
@@ -0,0 +1,12 @@
+class Task1:
+    def print_message(num):
+     if num> 10:
+         print("python conditions and loops are a piece of cake.")
+     else:
+         for_in range(num):
+         print("I am back to check on my skills!")
+
+    def main():
+        while True:
+            
+
---
+++ task66.py
@@ -0,0 +1,13 @@
+def find_shortest_longest_list():
+    l =["go","stay","play","laugh"]
+    shortest = l[0]
+    longest = l[0]
+    for i in l:
+        if len(i)< len(shortest):
+            shortest = i
+        elif len(i) > len(longest):
+            longest = i
+    return shortest, longest
+
+
+print(find_shortest_longest_list())
